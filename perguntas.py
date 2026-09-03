b3db8a147ae8786f756bfc9c1bacb39cf8ac76e3
Denisander Vivan <105618584+DenisPython1987@users.noreply.github.com>
2026-09-03 08:12:48 -0300

    Atualizar o perguntas.py

--- perguntas.py
+++ perguntas.py
@@ -70,22 +70,22 @@ def pergunta_1():
             return 1
             
 def pergunta_2():
-        opções = ("Broken Butterfly", "Standard Handgun", "Red 9", "Punisher")
-        certa = 0
-        print("\033[36mQual é a arma mais forte do jogo?\033[m")
-        linha()
-        for indice, valor in enumerate(opções):
-            if valor == "Broken Butterfly":
-                certa = indice + 1
-            print(f"\033[34m{indice + 1} - {valor}\033[m")
-        linha()
-        escolha = leia_int("\033[33mQual é a resposta correta? Sua resposta: \033[m")
-        linha()
-        validação = escolha_usuário(escolha)
-        if validação and escolha == certa:
-            return 3
-        if validação and escolha != certa:
-            return 1
+    opções = ("Broken Butterfly", "Standard Handgun", "Red 9", "Punisher")
+    certa = 0
+    print("\033[36mQual é a arma mais forte do jogo?\033[m")
+    linha()
+    for indice, valor in enumerate(opções):
+        if valor == "Broken Butterfly":
+            certa = indice + 1
+        print(f"\033[34m{indice + 1} - {valor}\033[m")
+    linha()
+    escolha = leia_int("\033[33mQual é a resposta correta? Sua resposta: \033[m")
+    linha()
+    validação = escolha_usuário(escolha)
+    if validação and escolha == certa:
+        return 3
+    if validação and escolha != certa:
+        return 1
             
 def pergunta_3():
         opções = ("Claire Redfield", "Ashley Graham", "Xuxa Meneghel", "Ada Wong")
